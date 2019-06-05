0bf53d37e06d98371e5548f7030a5b7ffb283562
Katerina Bosko <katerinabosko@gmail.com>
2019-06-05 12:08:32 -0700

    initial commit

---
+++ web_app/routes.py
@@ -0,0 +1,12 @@
+from web_app import app
+
+import json, plotly
+from flask import render_template
+#from flask import render_template, request, Response, jsonify
+#from scripts.data import return_figures
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
