9d1faa932aa60802ecfd01958fba5c2d64759f8b
Katerina Bosko <katerinabosko@gmail.com>
2019-06-05 16:32:02 -0700

    converted plotly viz to json and updated it in routes.py

--- web_app/routes.py
+++ web_app/routes.py
@@ -2,11 +2,19 @@ from web_app import app
 
 import json, plotly
 from flask import render_template
-#from flask import render_template, request, Response, jsonify
-from scripts.data import return_figures
+from web_app.scripts.data import return_figures
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+
+    figures = return_figures()
+
+    #plot ids for the html id tag
+    ids = [f'figure-{i}' for i, _ in enumerate(figures)]
+
+    #convert plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html', ids=ids, figuresJSON=figuresJSON)
 
